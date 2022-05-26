be290f0661d5c792115c66f362837d0d0d139f58
Naiem890 <solaiman.naiem890@gmail.com>
2022-05-26 20:18:25 +0600

    read review and post review added

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ async function run() {
 
     const toolsCollection = client.db("toolsDB").collection("toolsCollection");
     const orderCollection = client.db("toolsDB").collection("orderCollection");
+    const reviewCollection = client
+      .db("toolsDB")
+      .collection("reviewCollection");
 
     // Read all tools data
     app.get("/tools", async (req, res) => {
@@ -36,15 +39,44 @@ async function run() {
       res.send(tools);
     });
 
+    // Read tool data by id
     app.get("/tool/:id", async (req, res) => {
       const productId = req.params.id;
       const tool = await toolsCollection.findOne({ _id: ObjectId(productId) });
       res.send(tool);
     });
 
+    //Post order in to database
     app.post("/order", async (req, res) => {
       const order = await orderCollection.insertOne(req.body.order);
       res.send(order);
+    });
+
+    // Read all order data
+    app.get("/orders", async (req, res) => {
+      const orders = await orderCollection.find({}).toArray();
+      res.send(orders);
+    });
+
+    // Delete  order data
+    app.delete("/order/:id", async (req, res) => {
+      console.log(req.params.id);
+      const deletedOrder = await orderCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(deletedOrder);
+    });
+
+    //Post Review in to database
+    app.post("/review", async (req, res) => {
+      const review = await reviewCollection.insertOne(req.body.review);
+      res.send(review);
+    });
+
+    // Read all order data
+    app.get("/reviews", async (req, res) => {
+      const reviews = await reviewCollection.find({}).toArray();
+      res.send(reviews);
     });
   } finally {
     // Ensures that the client will close when you finish/error
