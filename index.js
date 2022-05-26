0d7273d8a5266a6d9a67e6b1169e167623a78153
Naiem890 <solaiman.naiem890@gmail.com>
2022-05-26 07:37:05 +0600

    database connected

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cavm6.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    console.log("Connected successfully to server");
+
+    const toolsCollection = client.db("toolsDB").collection("toolsCollection");
+
+    app.get("/tools", async (req, res) => {
+      const tools = await toolsCollection.find({}).toArray();
+      res.send(tools);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
